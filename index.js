55abdcbac62ccb9fb2e0bdd75fd060ec19076b08
Ryan <zhangry100@gmail.com>
2021-03-01 21:58:41 -0800

    Boilerplate bot now sends messages in channels

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const Discord = require("discord.js");
 const config = require("./config.json");
+const Discord = require("discord.js");
 const client = new Discord.Client();
 client.once("ready", () => {
 	console.log("Ready!");
+});
+client.on("message", message => {
+	if (message.content.substring(0, config.prefix.length) === config.prefix) {
+		message.channel.send("Hello. You have just said: **" + message.content.substring(config.prefix.length).trim() + "**.");
+	}
 });
 client.login(config.token);
