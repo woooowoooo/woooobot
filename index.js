6213d17dd5fab76804ab5a84db365a788c8d6d84
Ryan <zhangry100@gmail.com>
2021-03-03 01:03:45 -0800

    Cache my account
    The first step to sending all DM contents to me is to add my account.
    Also, the initialization message changed.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const {prefix, token, myID, botID} = require("./config.json");
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const me = client.users.fetch(myID);
 client.once("ready", () => {
-	console.log("Ready!");
+	console.log(`\n====================\nLogged in as ${client.user.tag}.\n`);
+	me.then((result) => {
+		console.log(result);
+		result.send("Initialized");
+		return result;
+	});
 });
 client.on("message", msg => {
 	if (msg.guild === null && msg.author.id != botID) { // Message is DM and not from the bot (to prevent a loop)
