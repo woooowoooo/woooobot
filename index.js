54fb87216291be2dd829f9e3115ac2c00b85f035
Ryan <zhangry100@gmail.com>
2021-03-03 00:50:42 -0800

    DM Log function
    Now direct messages are logged into the console. Next step is for the messages to be sent to me.

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ client.once("ready", () => {
 	console.log("Ready!");
 });
 client.on("message", msg => {
+	if (msg.guild === null && msg.author.id != botID) { // Message is DM and not from the bot (to prevent a loop)
+		console.log(`${msg.author.tag} said:\n${msg}`);
+	}
 	if (msg.content.substring(0, prefix.length) === prefix) {
 		msg.channel.send(`Your message: **${msg.content.substring(prefix.length)}**.`);
 	}
