8313dd2e59d3007b151f02662e34c34a494c7881
Ryan <zhangry100@gmail.com>
2021-03-10 23:51:44 -0800

    Log messages
    - Implemented a standardized after-message console log function.
    - Figured out partly how to work with Promises.
    - Made the two initializing lines to be of the same length.

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const {prefix, token, myID, botID} = require("./config.json");
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const me = client.users.fetch(myID);
+const me = client.users.fetch(myID).then(user => {
+	console.log(user);
+	return user;
+});
+function log(message) { // Log into console all woooobot messages
+	if (message.channel.type === "dm") {
+		console.log(`(DM) To ${message.channel.recipient.tag}: ${message}`);
+	}
+	else { // If it's not a DM it's probably a text channel.
+		console.log(`${message.channel.guild.name}, ${message.channel.name}: ${message}`);
+	}
+}
 client.once("ready", () => {
-	console.log(`\n====================\nLogged in as ${client.user.tag}.\n`);
-	me.then((result) => {
-		console.log(result);
-		result.send("Initialized");
-		return result;
-	});
+	console.log(`\n${'=' * (13 + client.user.tag.length)}\nLogged in as ${client.user.tag}.\n`);
+	me.then(user => user.send("Hello, world!").then(log));
 });
 client.on("message", msg => {
 	if (msg.guild === null && msg.author.id != botID) { // Message is DM and not from the bot (to prevent a loop)
-		console.log(`${msg.author.tag} said:\n${msg}`);
+		console.log(`${msg.author.tag}: ${msg}`);
 	}
 	if (msg.content.substring(0, prefix.length) === prefix) {
-		const reply = `Your message: **${msg.content.substring(prefix.length)}**.`;
-		console.log(`${client.user.tag}'s reply:\n${reply}`);
-		msg.channel.send(reply);
+		msg.channel.send(`Your message: **${msg.content.substring(prefix.length)}**.`).then(log);
 	}
 });
 client.login(token);
