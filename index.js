cf21423757f50ebd8845158f837c81dd7bafb71e
Ryan <zhangry100@gmail.com>
2021-03-11 00:33:14 -0800

    Two-line console logs
    - Changed logs to be two lines to be more readable.
    - Stopped DM-ing myself my own messages.
    - Fixed the '=' line bug ('=' would be NaN)

--- index.js
+++ index.js
@@ -7,20 +7,26 @@ const me = client.users.fetch(myID).then(user => {
 });
 function log(message) { // Log into console all woooobot messages
 	if (message.channel.type === "dm") {
-		console.log(`(DM) To ${message.channel.recipient.tag}: ${message}`);
+		console.log(`(DM) To ${message.channel.recipient.tag}\n	${message}`);
 	}
 	else { // If it's not a DM it's probably a text channel.
-		console.log(`${message.channel.guild.name}, ${message.channel.name}: ${message}`);
+		console.log(`${message.channel.guild.name}, ${message.channel.name}:\n	${message}`);
 	}
 }
 client.once("ready", () => {
-	console.log(`\n${'=' * (13 + client.user.tag.length)}\nLogged in as ${client.user.tag}.\n`);
-	me.then(user => user.send("Hello, world!").then(log));
+	console.log(`\n${'='.repeat(14 + client.user.tag.length)}\nLogged in as ${client.user.tag}.\n`);
 });
 client.on("message", msg => {
-	if (msg.guild === null && msg.author.id != botID) { // Message is DM and not from the bot (to prevent a loop)
-		console.log(`${msg.author.tag}: ${msg}`);
+	// Act on bot DMs
+	if (msg.guild === null && msg.author.id != botID) {
+		if (msg.author.id === myID) { // I know what I sent
+			console.log(`${msg.author.tag}:\n	${msg}`);
+		}
+		else {
+			me.then(user => user.send(`${msg.author.tag}: ${msg}`)).then(log);
+		}
 	}
+	// Act on server messages with the bot prefix
 	if (msg.content.substring(0, prefix.length) === prefix) {
 		msg.channel.send(`Your message: **${msg.content.substring(prefix.length)}**.`).then(log);
 	}
