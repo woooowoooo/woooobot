556e4cb533e5367d8a0fe37aae8cdcbeffd755e6
woooowoooo <51186839+woooowoooo@users.noreply.github.com>
2021-06-18 13:05:48 -0700

    Morshu v0.3
    - Implemented a more complex grammar.
        - Integrated non-final punctuation.
        - Added subcategories in the word lists.
    - Introduced weighted lists.
        - Weighted final punctuation.
    - Implemented an "any non-empty subset of set" function.
    - Implemented optional terms.
    - Added contractions.

--- morshu.js
+++ morshu.js
@@ -1,23 +1,57 @@
-// Word lists
+// Parts of speech
 const adjectives = ["long", "sorry", "little", "richer"];
-const adverbs = ["enough", "can", "not", "back", "when"];
-const determiners = ["yours", "enough", "my", "a"];
+const adverbs = ["enough", "back"];
+const conjunctions = ["as long as", "when"];
+const determiners = ["enough", "my", "a"];
 const interjections = ["mmm"];
-const nouns = ["lamp", "oil", "rope", "bombs", "friend", "rubies", "rupees", "link", "credit", "back"];
-const properNouns = ["you", "it", "enough", "Link", "I"]; // Also includes pronouns
 const prepositions = ["as", "when"];
-const verbs = ["want", "is", "have", "give", "come", "are"];
-// Punctuation
-const punct = [", ", "; ", ": ", " — "];
-const punctFinal = ["… ", "! ", "? ", "‽ "];
+const nouns = {
+	singular: ["lamp", "rope", "friend", "link", "credit", "back"],
+	plural: ["bombs", "rubies", "rupees"],
+	mass: ["oil"],
+	proper: ["it", "yours", "enough", "Link"],
+	pronoun: ["you", "I"] // "it" is also a pronoun but it is singular
+};
+const verbs = {
+	singular: ["is"],
+	plural: ["want", "are", "have", "give", "come"],
+	modal: ["can", "can not"]
+};
 // Chance variables
-const properChance = 0.5; // Chance of getting a proper noun or pronoun
-const adjectiveChance = 0.4; // Chance of an extra adjective or adverb
-const punctSpecialChance = 0.5; // Chance of a special punctuation instead of a period
-const multiClauseChance = 0.2; // Chance of a complex sentence (multiple clauses)
+const nounAdjunctChance = 0.2; // Chance of a noun phrase having a noun adjunct
+const modalVerbChance = 0.3; // Chance of a verb being modified by a modal verb
+const adjectiveChance = 0.3; // Chance of an extra adjective or adverb
+const nounPhraseChance = 0.2; // Chance of a verb phrase having a noun phrase
+const contractionChance = 0.5; // Chance of a potential contraction happening
+// Other variables
+const punctuation = [
+	[0.5, "."],
+	[0.2, "!"],
+	[0.15, "?"],
+	[0.1, "…"],
+	[0.05, "‽"]
+];
+const contractions = [
+	["it is", "it's"],
+	["can not", "can't"],
+	["you are", "you're"]
+];
+let number = "singular";
 // Helper functions
 function choose(array) {
 	return array[Math.floor(Math.random() * array.length)];
+}
+function chooseWeighted(array, terminals = false) {
+	const chance = Math.random();
+	let previous = 0;
+	for (const option of array) {
+		if (chance < previous + option[0]) {
+			return terminals ? option[1] : option[1]();
+		}
+		previous += option[0];
+	}
+	const last = array.pop();
+	return terminals ? last[1] : last[1](); // Last element
 }
 function roll(chance) {
 	return Math.random() < chance;
@@ -25,39 +59,77 @@ function roll(chance) {
 function capitalize(word) {
 	return word.charAt(0).toUpperCase() + word.substring(1);
 }
+function oneOrMore(options, chance, separator = true, generator = false) {
+	const option = choose(options);
+	let text = generator ? options() : option;
+	if (roll(chance) && (generator || (!generator && options.length > 1))) {
+		text += separator ? ", " : " ";
+		if (!generator) {
+			options.splice(options.indexOf(option), 1); // Prevent duplicates
+		}
+		text += oneOrMore(options, chance, separator, generator);
+	}
+	return text;
+}
+function optional(chance, generator, appendSpace = false) {
+	if (!roll(chance)) {
+		return "";
+	}
+	return appendSpace ? generator() + " " : " " + generator();
+}
 // Generation
 function genNounPhrase() {
-	if (roll(properChance)) {
-		return choose(properNouns);
-	}
-	let nounPhrase = choose(determiners) + " ";
-	if (roll(adjectiveChance)) {
-		nounPhrase += choose(adjectives) + " ";
-	}
-	nounPhrase += choose(nouns);
-	return nounPhrase;
+	const subs = [
+		[0.4, () => { // Singular noun
+			number = "singular";
+			return choose(determiners) + optional(adjectiveChance, () => oneOrMore(adjectives, adjectiveChance)) + " " + oneOrMore(nouns.singular, nounAdjunctChance, false);
+		}],
+		[0.2, () => { // Plural noun
+			number = "plural";
+			return optional(adjectiveChance, () => oneOrMore(adjectives, adjectiveChance), true) + optional(nounAdjunctChance, () => oneOrMore(nouns.singular, nounAdjunctChance, false), true) + choose(nouns.plural);
+		}],
+		[0.2, () => { // Pronoun that is not "he", "she", or "it"
+			number = "plural";
+			return choose(nouns.pronoun);
+		}],
+		[0.1, () => { // Proper noun
+			number = "singular";
+			return choose(nouns.proper);
+		}],
+		[0.1, () => { // Mass noun
+			number = "singular";
+			return optional(adjectiveChance, () => oneOrMore(adjectives, adjectiveChance), true) + oneOrMore(nouns.mass, nounAdjunctChance, false);
+		}]
+	];
+	return chooseWeighted(subs);
 }
 function genVerbPhrase() {
-	if (roll(adjectiveChance)) {
-		return choose(adverbs) + " " + genVerbPhrase();
-	}
-	return choose(verbs) + " " + genNounPhrase();
+	return optional(modalVerbChance, () => choose(verbs.modal), true) +
+		choose(verbs[number]) +
+		optional(adjectiveChance, () => oneOrMore(adverbs, adjectiveChance)) +
+		optional(nounPhraseChance, genNounPhrase);
 }
 function genClause() {
 	return genNounPhrase() + " " + genVerbPhrase();
 }
+function genSubClause() {
+	return choose(prepositions) + " " + genClause();
+}
 function genSentence() {
-	let sentence = genClause();
-	if (roll(multiClauseChance)) {
-		sentence += choose(punct);
-		sentence += genClause();
+	const subs = [
+		[0.6, () => genClause()],
+		[0.1, () => genClause() + choose([": ", "; ", `, ${choose(conjunctions)} `]) + genClause()],
+		[0.1, () => genClause() + " " + genSubClause()],
+		[0.1, () => genSubClause() + ", " + genClause()],
+		[0.1, () => genNounPhrase() + ": " + genClause()]
+	];
+	let sentence = chooseWeighted(subs);
+	for (const contraction of contractions) {
+		if (roll(contractionChance)) {
+			sentence = sentence.replaceAll(contraction[0], contraction[1]);
+		}
 	}
-	if (roll(punctSpecialChance)) {
-		sentence += choose(punctFinal);
-	} else {
-		sentence += ".";
-	}
-	return sentence;
+	return sentence + chooseWeighted(punctuation, true);
 }
 exports.generate = function (sentenceAmount) {
 	let sentences = [];
