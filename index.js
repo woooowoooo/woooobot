e4e6710f8d057c78086db22a4c129d60cd139f21
woooowoooo <51186839+woooowoooo@users.noreply.github.com>
2021-12-05 23:49:49 -0800

    Bot channel message ignorance bug fix
    Discord.js v13's mandatory intent declarations got the best of me.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const {Client, Intents} = require("discord.js");
 const client = new Client({
 	intents: [
+		Intents.FLAGS.GUILDS,
 		Intents.FLAGS.GUILD_MEMBERS,
 		Intents.FLAGS.GUILD_MESSAGES,
 		Intents.FLAGS.DIRECT_MESSAGES
