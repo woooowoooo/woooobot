0a7fd653170430afc86e9465e506b974dae9669a
woooowoooo <51186839+woooowoooo@users.noreply.github.com>
2022-01-17 11:54:44 -0800

    Rudimentary results
    NEEDS TESTING!

--- voting.js
+++ voting.js
@@ -6,7 +6,7 @@ const {twowPath} = require("./config.json"); // TODO: Add support for multiple T
 const status = require(twowPath + "status.json");
 const {currentRound, seasonPath, roundPath} = status;
 const {
-	roles: {alive, remind},
+	roles: {remind},
 	channels: {bots, voting, reminders: remindersId, results: resultsId}
 } = require(twowPath + "twowConfig.json");
 // Season-specific
@@ -88,7 +88,7 @@ exports.initVoting = function () {
 	status.phase = "voting";
 	save(`${twowPath}/status.json`, status);
 	const unixDeadline = toUnixTime(vDeadline);
-	sendMessage(voting, `<@&${alive}> ${currentRound}\nVote to <@814748906046226442> by <t:${unixDeadline}> (<t:${unixDeadline}:R>)`, true);
+	sendMessage(voting, `@everyone ${currentRound}\nVote to <@814748906046226442> by <t:${unixDeadline}> (<t:${unixDeadline}:R>)`, true);
 	// Create voting
 	logMessage(prompt);
 	const screenSizes = partitionResponses(responses.length);
@@ -161,9 +161,55 @@ exports.logVote = function (message) {
 	return `Your vote has been recorded:\n\`\`\`${voteFull.map(matches => matches[0]).join("\n")}\`\`\`${votes[message.author.id].supervote ? "Thank you for supervoting!" : ""}`;
 };
 exports.results = function () {
-	// TODO: Create results
-	// Spoiler wall
-	for (let i = 0; i < 50; i++) {
-		sendMessage(results, morshu(1), true);
+	logMessage("Results started.");
+	// TODO: Calculate results
+	const rankings = [];
+	for (const response of responses) {
+		const ratings = Array.from(Object.values(response.ratings));
+		const percentage = ratings.reduce((a, b) => a + b, 0) / ratings.length;
+		const stDev = 0; // TODO: Figure out how to calculate
+		const skew = 0; // TODO: Figure out how to calculate
+		rankings.push({
+			type: "hi", // TODO: Figure out how to calculate
+			rank: 0, // TODO: Figure out how to calculate
+			book: "path", // TODO: Figure out how to calculate
+			name: "name", // TODO: Figure out how to calculate
+			response: response.text,
+			percentage: percentage * 100,
+			stDev: stDev * 100,
+			skew: skew,
+			votes: ratings.length
+		});
 	}
+	// Reveal results
+	async function revealSlide(line) {
+		line = line.toString().trim();
+		if (line === "stop") {
+			stdin.removeListener("data", revealSlide);
+			// Full leaderboard
+			const path = `leaderboard.png`;
+			await sendSlide(path, true);
+			// Spoiler wall
+			for (let i = 0; i < 50; i++) {
+				sendMessage(resultsId, morshu(1), true);
+			}
+			return;
+		}
+		const path = `slide${slide}.png`;
+		await sendSlide(path, (slide === 1));
+		slide++;
+	}
+	async function sendSlide(path, header) {
+		// TODO: Use round name
+		await drawResults(`${roundPath}/results/${path}`, "Round 1", prompt, rankings, header);
+		sendMessage(resultsId, {
+			files: [{
+				attachment: `${roundPath}/results/${path}`,
+				name: path
+			}]
+		}, true);
+	}
+	let slide = 1;
+	let stdin = process.openStdin();
+	stdin.addListener("data", revealSlide); // Enter to reveal
 };
