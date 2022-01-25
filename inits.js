17354a33cc37ef9ee5bc4d8e9921003de1b23ce0
woooowoooo <51186839+woooowoooo@users.noreply.github.com>
2022-01-25 03:09:17 -0800

    Init changes
    - Fully-fledged next round config setting
    - Implemented creating new files in initSeason
    - Combined the new round functions

--- inits.js
+++ inits.js
@@ -5,59 +5,63 @@ const {logMessage, getTime, toUnixTime, save} = require("./helpers.js");
 const {twowPath} = require("./config.json"); // TODO: Add support for multiple TWOWs
 const status = require(twowPath + "status.json");
 let {currentSeason, currentRound, seasonPath, roundPath} = status;
-const {seasons} = require(twowPath + "twowConfig.json");
+const {seasons, nextSeason = {}} = require(twowPath + "twowConfig.json");
 // Season-specific
-const {rounds, nextPrompt} = require(seasonPath + "seasonConfig.json");
+const {rounds, nextRound = {}} = require(seasonPath + "seasonConfig.json");
 const seasonConfig = require(seasonPath + "seasonConfig.json");
 const seasonContestants = require(seasonPath + "seasonContestants.json");
 // Round-specific
 const roundConfig = require(roundPath + "roundConfig.json");
 const contestants = require(roundPath + "contestants.json");
-async function newRound() {
+exports.initRound = async function (newRoundName) {
+	// Start new round
+	currentRound = newRoundName ?? Object.keys(rounds)[Object.keys(rounds).indexOf(currentRound) + 1];
+	roundPath = seasonPath + rounds[currentRound];
+	status.currentRound = currentRound;
+	status.roundPath = roundPath;
+	await save(twowPath + "status.json", status);
+	logMessage("New round started: " + currentRound);
 	// Create new files
 	await fs.mkdir(roundPath);
 	await fs.mkdir(roundPath + "results/");
 	await fs.mkdir(roundPath + "screens/");
-	roundConfig.round = currentRound;
-	roundConfig.prompt = nextPrompt ?? "";
-	roundConfig.rDeadline = getTime(toUnixTime(roundConfig.vDeadline) + seasonConfig.deadlines[0] * 86400);
-	roundConfig.vDeadline = getTime(toUnixTime(roundConfig.rDeadline) + seasonConfig.deadlines[1] * 86400);
+	Object.assign(roundConfig, {
+		round: currentRound,
+		prompt: "",
+		rDeadline: getTime(toUnixTime(roundConfig.vDeadline) + seasonConfig.deadlines[0] * 86400),
+		vDeadline: getTime(toUnixTime(roundConfig.rDeadline) + seasonConfig.deadlines[1] * 86400)
+	}, nextRound);
 	await save(roundPath + "roundConfig.json", roundConfig);
 	contestants.responseCount = {};
 	await save(roundPath + "contestants.json", contestants);
 	await save(roundPath + "responses.json", []);
 	await save(roundPath + "votes.json", {});
 	await save(roundPath + "screens.json", {});
-}
-exports.initRound = async function () {
-	// Start new round
-	// const oldPath = roundPath;
-	const roundNames = Object.keys(rounds);
-	currentRound = roundNames[roundNames.indexOf(currentRound) + 1];
-	roundPath = seasonPath + rounds[currentRound];
-	status.currentRound = currentRound;
-	status.roundPath = roundPath;
-	await save(twowPath + "status.json", status);
-	logMessage("New round started: " + currentRound);
-	await newRound();
 };
 exports.initSeason = async function () {
 	// Start new season
+	const oldPath = seasonPath;
 	const seasonNames = Object.keys(seasons);
 	currentSeason = seasonNames[seasonNames.indexOf(currentSeason) + 1];
 	seasonPath = twowPath + seasons[currentSeason];
-	currentRound = "Round 1";
-	roundPath = seasonPath + "Round 1/";
 	status.currentSeason = currentSeason;
-	status.seasonPath = seasonPath;
-	status.currentRound = currentRound;
-	status.roundPath = roundPath; // Maybe combine last few lines with double assignments?
+	status.seasonPath = seasonPath; // Maybe combine last few lines with double assignments?
 	await save(twowPath + "status.json", status);
 	logMessage("New season started: " + currentSeason);
-	// TODO: Create new files
+	// Create new files
 	await fs.mkdir(seasonPath);
 	await fs.mkdir(seasonPath + "books/");
+	await fs.copyFile(oldPath + "graphics.js", seasonPath + "graphics.js");
+	Object.assign(seasonConfig, {
+		season: currentSeason,
+		rounds: {
+			"Round 1": "Round 1/"
+		}
+	}, nextSeason);
+	await save(seasonPath + "seasonConfig.json", seasonConfig);
+	seasonContestants.names = {};
+	seasonContestants.bookPaths = {};
+	await save(seasonPath + "seasonContestants.json", seasonContestants);
 	// Start Round 1
-	newRound();
-	// TODO: Specific changes for Round 1
+	await exports.initRound("Round 1"); // TODO: Specific changes for Round 1
 };
