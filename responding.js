ec018cf15177bb9d154611947198fc603c99d36c
woooowoooo <51186839+woooowoooo@users.noreply.github.com>
2022-02-25 22:18:49 -0800

    Generalize Round 1 joins
    Now any round can be marked as a "join" round. This complements the "dummy" property.

--- responding.js
+++ responding.js
@@ -10,11 +10,11 @@ const {
 	channels: {bots, prompts, reminders: remindersId}
 } = require(twowPath + "twowConfig.json");
 // Season-specific
-const {deadlines, reminders, dummies} = require(seasonPath + "seasonConfig.json");
+const {deadlines, reminders, joins: _j, dummies: _d} = require(seasonPath + "seasonConfig.json");
 const technicals = require(seasonPath + "technicals.js");
 const twists = require(seasonPath + "twists.js");
 // Round-specific
-const {prompt, example, rDeadline, technicals: roundTechnicals = [], twists: roundTwists} = require(roundPath + "roundConfig.json");
+const {prompt, example, rDeadline, technicals: roundTechnicals = [], twists: roundTwists, joins = _j, dummies = _d} = require(roundPath + "roundConfig.json");
 const contestants = require(roundPath + "contestants.json");
 const responses = require(roundPath + "responses.json");
 // Functions
@@ -26,18 +26,22 @@ exports.initResponding = async function () {
 	await sendMessage(prompts, `<@&${aliveId}> ${status.currentRound} Prompt:\`\`\`\n${prompt}\`\`\`Respond to <@814748906046226442> by <t:${unixDeadline}> (<t:${unixDeadline}:R>)\nHere's an example response: \`${example ?? ""}\``, true);
 };
 exports.logResponse = function (message) {
+	// Reject extra responses and determine dummies
+	// TODO: Allow edits
 	logMessage(`Recording response by ${message.author}:\n\t${message}`);
 	const prized = contestants.prize.includes(message.author.id);
 	const alive = contestants.alive.includes(message.author.id);
-	const isDummy = status.currentRound !== "Round 1" && !(prized || alive);
-	if (!dummies && isDummy) {
-		return "Dummy/filler responses are currently not accepted.";
-	}
 	const allowed = prized ? 3 : 2; // Temporary edit for this season
-	contestants.responseCount[message.author.id] ??= 0;
-	// TODO: Allow edits
-	if (contestants.responseCount[message.author.id] >= allowed) {
-		return `You have already responded to the prompt!`;
+	const isAllowed = (contestants.responseCount[message.author.id] ?? 0) < allowed;
+	let isDummy = false;
+	if (!prized && !alive && !joins || !isAllowed) {
+		if (!dummies) {
+			if (prized || alive) {
+				return `You have already responded to the prompt!`;
+			}
+			return "Non-contestant responses are currently not accepted.";
+		}
+		isDummy = true;
 	}
 	// Default tenWords technical
 	function tenWord(response) {
@@ -65,13 +69,15 @@ exports.logResponse = function (message) {
 		}, message.content);
 	}
 	responses.push(messageData);
-	if (status.currentRound === "Round 1") {
+	// Stuff
+	if (status.currentRound === "Round 1") { // TODO: Allow rejoin rounds
 		// Do Round 1 stuff
 		contestants.alive.push(message.author.id);
 		addRole(serverId, message.author.id, aliveId);
 	}
+	contestants.responseCount[message.author.id] ??= 0;
 	contestants.responseCount[message.author.id]++;
 	save(`${roundPath}/responses.json`, responses);
 	save(`${roundPath}/contestants.json`, contestants);
-	return `Your response (\`${message}\`) has been recorded. Your response is response #${responses.length}`;
+	return `Your response (\`${message}\`) has been recorded${isDummy ? " as a dummy. **This means that its placement in results does not matter.**" : "."} Your response is response #${responses.length}`;
 };
