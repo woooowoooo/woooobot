43725df9b4a81c8f6f5fd8c52c8e8aacbc68c4c0
woooowoooo <51186839+woooowoooo@users.noreply.github.com>
2023-01-03 11:21:59 -0800

    Allow MessageContent intent
    Discord API moment (DMs worked, server messages didn't)

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const client = new Client({
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildMembers,
 		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.DirectMessages
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.MessageContent
 	],
 	partials: [
 		Partials.Channel
