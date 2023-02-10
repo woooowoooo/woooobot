f25461ad2c66ed8052b3cfba49002cf3ac0484d1
woooowoooo <51186839+woooowoooo@users.noreply.github.com>
2023-02-09 16:07:40 -0800

    Add `listSeasonContestants`
    Lists season contestants raw

--- statistics.js
+++ statistics.js
@@ -1,6 +1,12 @@
 const {twowPath} = require("./config.json");
 const {seasonPath} = require(twowPath + "status.json");
 const {rounds} = require(seasonPath + "seasonConfig.json");
+function listSeasonContestants() {
+	const {names} = require(seasonPath + "seasonContestants.json");
+	for (let name of Object.values(names)) {
+		console.log(name);
+	}
+}
 function listPrompts() {
 	for (let roundPath of Object.values(rounds)) {
 		const {prompt} = require(seasonPath + roundPath + "roundConfig.json");
