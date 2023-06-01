7855dd39f7c999986afa547e57360e543fdc8c68
woooowoooo <51186839+woooowoooo@users.noreply.github.com>
2023-05-31 19:41:03 -0700

    Separate round-specific stats
    Organization purposes

--- statistics.js
+++ statistics.js
@@ -8,20 +8,21 @@ function listSeasonContestants() {
 		console.log(name);
 	}
 }
-function listRoundContestants(round) {
+function listPrompts() {
+	for (let seasonPath of Object.values(seasons)) {
+		const {rounds} = require(twowPath + seasonPath + "seasonConfig.json");
+		for (let roundPath of Object.values(rounds)) {
+			const {prompt} = require(twowPath + seasonPath + roundPath + "roundConfig.json");
+			console.log(prompt);
+		}
+	}
+}
+// Round-specific
+function listContestants(round) {
 	const {names} = require(seasonPath + "seasonContestants.json");
 	const {responseCount} = require(seasonPath + rounds[round] + "contestants.json");
 	for (let id of Object.keys(responseCount)) {
 		console.log(names[id]);
-	}
-}
-function listPrompts() {
-	for (let seasonPatha of Object.values(seasons)) {
-		const {rounds} = require(twowPath + seasonPatha + "seasonConfig.json");
-		for (let roundPath of Object.values(rounds)) {
-			const {prompt} = require(twowPath + seasonPatha + roundPath + "roundConfig.json");
-			console.log(prompt);
-		}
 	}
 }
 Object.assign(exports, {showContestants, listSeasonContestants, listRoundContestants, listPrompts});
